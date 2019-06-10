bc0d1e09a9511780dda0d380dce67d032170d579
s33dco <code@s33d.co>
2019-06-10 19:05:27 +0100

    init commit

---
+++ lib/bbc10.rb
@@ -0,0 +1,19 @@
+require "bbc10/version"
+require "open-uri"
+require 'nokogiri'
+
+module Bbcmostread
+  class Error < StandardError; end
+
+  url = 'https://www.bbc.co.uk/news'
+
+  doc = Nokogiri::HTML(open(url).read)
+
+  links = doc.css('.nw-c-most-read__items a').map{|link| 'https://www.bbc.co.uk/' << link['href']}
+
+  links.each do | link |
+    puts "\n\n#{link}\n\n"
+    story = Nokogiri::HTML(open(link).read)
+    story.css('.story-body__inner p').map(&:content).each{|para| puts "#{para}\n\n"}
+  end
+end
---
+++ lib/bbc10/version.rb
@@ -0,0 +1,3 @@
+module Bbc10
+  VERSION = "0.1.0"
+end
